6727b3cc6e1efe246a59e4aaec8066d65ba51211
abhijeethub <abhijeetbisht2020@gmail.com>
2020-06-12 18:04:24 +0530

    added abhijeet comment

---
+++ python/read_image/read_image.py
@@ -0,0 +1,9 @@
+import matplotlib.image as mi
+import matplotlib.pyplot as plt
+
+#Comment - Akshay
+#comment - Abhijeet
+image = mi.imread("rgb.png")
+print(image) #extra
+plt.imshow(image)
+plt.show()
