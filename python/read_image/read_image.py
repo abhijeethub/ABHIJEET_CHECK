56545bf8da01cefa7d53bd5bd3affb28fb05f958
ABHIJEETBISHT <akkinitj@gmail.com>
2020-06-13 10:44:57 +0530

    Added type

--- python/read_image/read_image.py
+++ python/read_image/read_image.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 #Comment - Akshay
 #comment - Abhijeet
 image = mi.imread("rgb.png")
+print(f"Image type is {type(image)}")
 print(image) #extra
 plt.imshow(image)
 plt.show()
