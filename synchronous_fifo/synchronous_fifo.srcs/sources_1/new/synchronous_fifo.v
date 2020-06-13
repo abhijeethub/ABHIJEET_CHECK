41f7f899b797bb08e81888dfb3e60dd406024714
ABHIJEETBISHT <akkinitj@gmail.com>
2020-06-13 11:40:44 +0530

    separated ports per clock domain

--- synchronous_fifo/synchronous_fifo.srcs/sources_1/new/synchronous_fifo.v
+++ synchronous_fifo/synchronous_fifo.srcs/sources_1/new/synchronous_fifo.v
@@ -1,36 +1,21 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/10/2020 10:30:53 PM
-// Design Name: 
-// Module Name: synchronous_fifo
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module synchronous_fifo(clk,reset,read_en,write_en,data_in,data_out,occupancy,empty,full);
 
 parameter DATA_SIZE = 32;
-parameter DEPTH  = 256;
+parameter DEPTH  = 16; //Keeping depth less to see rollover early
 
-input clk,reset,read_en,write_en;
+//Write clock domain
+input clk,reset,write_en;
 input [DATA_SIZE - 1 : 0]data_in;
+output full;
+
+//Read clock domain
+input read_en;
 output reg [DATA_SIZE - 1 : 0]data_out;
 output reg [$clog2(DEPTH) - 1 : 0]occupancy; //Great use of clog2
-output reg empty,full;
+output reg empty;
 
-always@(posedge clk);
+
 
 endmodule
