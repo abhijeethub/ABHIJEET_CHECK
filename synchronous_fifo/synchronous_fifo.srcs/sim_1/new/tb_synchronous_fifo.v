2df4eace31ccba2361ccff43bee6c529453c85cc
abhijeethub <abhijeetbisht2020@gmail.com>
2020-06-17 22:54:28 +0530

    final commit

--- synchronous_fifo/synchronous_fifo.srcs/sim_1/new/tb_synchronous_fifo.v
+++ synchronous_fifo/synchronous_fifo.srcs/sim_1/new/tb_synchronous_fifo.v
@@ -1,47 +1,40 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/13/2020 10:52:01 AM
-// Design Name: 
-// Module Name: tb_synchronous_fifo
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
+`timescale 1ps / 1ps
 module tb_synchronous_fifo();
 
-parameter DATA_SIZE = 8;
+`define TIMEOUT 40000
+parameter DATA_SIZE = 32;
 parameter DEPTH  = 16; //Keeping depth less to see rollover early
 
 reg tb_clk,tb_reset,tb_read_en,tb_write_en;
 reg [DATA_SIZE-1 : 0]tb_data_in;
 
 wire [DATA_SIZE-1:0]tb_data_out;
-wire [$clog2(DEPTH):0]tb_occupancy;
+wire tb_data_out_valid;
+wire [$clog2(DEPTH):0]tb_available_entries;
 wire tb_empty,tb_full; 
+
+reg [DATA_SIZE -1 : 0]write_array[0:1000];
+reg [DATA_SIZE -1 : 0]read_array[0:1000];
+integer write_index = 0;
+integer read_index = 0;
+integer errors = 0;
+reg [4*8-1:0] status; //Storing a string - each character takes 8-bits (PASS - takes 4*8 bits)
+						//string always stored as reg in verilog, there is no key word string.
+
 
 synchronous_fifo inst (.clk(tb_clk),.reset(tb_reset),
 .read_en(tb_read_en),.write_en(tb_write_en),
-.data_in(tb_data_in),.data_out(tb_data_out),
-.occupancy(tb_occupancy),.empty(tb_empty),
+.data_in(tb_data_in),.data_out(tb_data_out),.data_out_valid(tb_data_out_valid),
+.available_entries(tb_available_entries),.empty(tb_empty),
 .full(tb_full));
 
 initial
 begin
  tb_clk = 0;
  tb_reset = 1;
+ tb_read_en = 0;
+ tb_write_en = 0;
+ tb_data_in = 0;
 end 
  
 always #5 tb_clk = !tb_clk;
@@ -50,36 +43,61 @@ initial
     begin
      #20 tb_reset = 0;
      
-	 repeat (4)
+	 repeat(5000)  // write_fifo will be called 16 times.
 	 begin
-	   @(posedge tb_clk);
-	   write_fifo($random);
+	   @(posedge tb_clk);  //this line is added after declaration of task write_fifo, coz in delclrn. tb_write_en signal of
+                             //	   first repeat is '0' and that signal is being '1' in second repeat at same edge. so to 
+							 //differentiate this, we added clock-delay.
+		if($random%2 == 0)			 		 
+		if(tb_full != 1)				   
+			write_fifo($random);  //task call; NOTE:-task call can conain argument, but task declaration can't.
 	 end
-	 #100;
-	 repeat (2)
+	end 
+	 //#100; //read after certain delay
+	 
+	 initial
+	  begin
+	    @(!tb_reset); //Block till I am not out of reset
+		repeat (5000) //read_fifo will be called 5 times.	
+		begin
+		@(posedge tb_clk); //this line is added after declaration of task write_fifo, coz in delclrn. tb_write_en signal of first 
+	                       //repeat is '0' and that signal is being '1' in second repeat at same edge. so to differentiate 
+						    //this, we added clock-delay.
+		if($random%2 == 0)	
+		read_fifo; //here no need of argument; coz we are not giving external input.
+		end
+	  end
+	 /*repeat (10)
 	 begin
-	   @(posedge tb_clk);
-	   read_fifo;
-	 end
+	   @(posedge tb_clk);  //this line is added after declaration of task write_fifo, coz in delclrn. tb_write_en signal of first repeat is '0' and that signal
+		if(tb_full != 1)				   //is being '1' in second repeat at same edge. so to differentiate this, we added clock-delay.
+			write_fifo($random);
+	 end*/
+	 
 	
-   end
+   
  
-task write_fifo;
-	input [DATA_SIZE-1:0]data;
+task write_fifo; //we can't give argument in task declaration.
+	input [DATA_SIZE-1:0]data; //we use this(input/output declaration) , instead of giving argument.
     begin
-        $display("input data is %h",data); 
-        tb_data_in = data;
-        $display("input TB data is %h",tb_data_in);
+        tb_data_in = data;     //TASK DECLARATION IS DOING 2 THINGS:- 1.giving data given by argument of task-call to tb_data_in.
+		                                                               //2.enabeling the write signal.
+        $display("Write TB data is %h",tb_data_in);
         if(tb_full != 1)
+		begin
+			write_array[write_index] = data;
+			write_index = write_index+ 1;
             tb_write_en = 1'b1;
-        @(posedge tb_clk);
-        tb_write_en = 0;
+		end	
+        @(posedge tb_clk); //v. imp.  means.. wait for 1 posedge, we also used semicolon here ..coz its independt statement.
+        tb_write_en = 0; //Down this signal after 1 clock edge. We are putting it down... coz we want to observe individual
+		                  //observation of every write, if we would keep it always 1..we cant observe indivdly.
     end
 endtask
 
 task read_fifo;
     begin
-        $display("input TB data is %h",tb_data_in);
+       // $display("input TB data is %h",tb_data_in);
         if(tb_empty != 1)
             tb_read_en = 1'b1;
 		else
@@ -88,15 +106,43 @@ task read_fifo;
 		@(posedge tb_clk);
         tb_read_en = 0;
 		@(posedge tb_clk);
-		$display("Read value is %h",tb_data_out);
+		if(tb_data_out_valid)  //we want to observe our output , only at valid-bit = 1..
+			begin
+				$display("Read value is %h",tb_data_out);
+				read_array[read_index] = tb_data_out;
+				read_index = read_index + 1;
+			end
     end
 endtask 
- 
- 
+ initial
+	begin
+	   #(`TIMEOUT-2)
+		$display("Reached here - Summing up !");
+		for(integer i = 0;i< read_index;i = i + 1)
+		 begin			
+			if (write_array[i] == read_array[i])
+				status = "PASS";
+			else
+			begin
+				status = "FAIL";
+				errors = errors + 1;
+			end
+			
+			$display("write data = %h,read data = %h,%s",write_array[i],read_array[i],status);
+		 end
+		
+		
+	end
+/*  initial
+ while(1)
+ $display("Time = %t",$time); */
  
  initial 
    begin
-    #400 $finish;
+    #(`TIMEOUT)
+	$display("Total errors:%d/%d",errors,read_index);
+	$display("Time = %t,Reached here - finishing !",$time);
+	$finish;
    end   
 
 endmodule
--- synchronous_fifo/synchronous_fifo.srcs/sources_1/new/synchronous_fifo.v
+++ synchronous_fifo/synchronous_fifo.srcs/sources_1/new/synchronous_fifo.v
@@ -1,9 +1,9 @@
-`timescale 1ns / 1ps
+//`timescale 1ns / 1ns
 
-module synchronous_fifo(clk,reset,read_en,write_en,data_in,data_out,occupancy,empty,full);
+module synchronous_fifo(clk,reset,read_en,write_en,data_in,data_out,data_out_valid,available_entries,empty,full);
 
 
-parameter DATA_SIZE = 8;
+parameter DATA_SIZE = 32;
 parameter DEPTH  = 16; //Keeping depth less to see rollover early
 
 
@@ -12,13 +12,14 @@ input clk,reset;
 //Write clock domain
 input write_en;
 input [DATA_SIZE - 1 : 0]data_in;
-output reg full;
+output  full;
 
 //Read clock domain
 input read_en;
 output reg [DATA_SIZE - 1 : 0]data_out;
-output  [$clog2(DEPTH) : 0]occupancy; //Great use of clog2
-output reg empty;
+output reg data_out_valid;
+output  [$clog2(DEPTH) : 0]available_entries; //Great use of clog2
+output  empty;
 
 
 reg [DATA_SIZE-1:0]mem[0:DEPTH-1];
@@ -27,34 +28,89 @@ wire write_permitted;
 wire read_permitted;
 reg [$clog2(DEPTH)-1:0]write_pointer;
 reg [$clog2(DEPTH)-1:0]read_pointer;
+reg roll_over;
 
-assign write_permitted = (write_en && !full) || (write_en && full && read_en);
-assign read_permitted = (read_en && !empty) || (read_en && empty && write_en);
-assign occupancy = DEPTH - write_pointer;
+//extra code for status register
+/*reg [7:0]status_register;
+
+// 7 - Full
+// 6 - Empty
+// 5 - x
+// 4:0 - Available entries
+assign status_register[7] = full;
+assign status_register[6] = empty;
+assign status_register[4:0] = available_entries;*/
+
+assign write_permitted = (write_en && !full);// || (write_en && full && read_en);
+assign read_permitted = (read_en && !empty);// || (read_en && empty && write_en);
+assign available_entries = roll_over ? (read_pointer - write_pointer) : (DEPTH - (write_pointer - read_pointer)); 
+                                                        //write_pointer - read_pointer will give occupied region.
+
+                   //it will not come inside always block; coz it should be clock independent,
+				   //so that available_entries would be independent
+				   //of clock
+//Empty and Full flags
+assign empty = (write_pointer - read_pointer ==0) && !roll_over; //visualize and draw...take time
+assign full =  (write_pointer - read_pointer ==0) && roll_over; //visualize and draw..take time
+                                                            //read cant lead by write pointer even in roll-over condition.
 
 always@(posedge clk,posedge reset)
 if (reset)
     begin
         data_out <= 0;
-        empty  <= 0;
-        full <= 0;
+		data_out_valid <= 0;
         write_pointer <=0;
         read_pointer <=0;
+		roll_over <= 1'b0;
     end
 else
     begin
-        if(write_permitted)
+		//Read-Write in same cycle
+		if(write_permitted && read_permitted)
+			begin
+				write_pointer <= write_pointer + 1;
+                mem[write_pointer] <= data_in;
+				read_pointer <= read_pointer + 1;
+                data_out <= mem[read_pointer];
+				data_out_valid <= 1'b1; //Akshay
+				
+				if(write_pointer == DEPTH-1 && read_pointer != (DEPTH-1) )//way to handel roll-over.
+					roll_over <= 1'b1; //above if contain only this statement, coz we didnt use 'begin-end'.
+					
+				if(read_pointer == DEPTH-1 && write_pointer !=(DEPTH-1)) //
+					roll_over <= 1'b0;//above if contain only this statement, coz we didnt use 'begin-end'.
+					                   //its not possbl that, read will roll_over and write will not roll-over.
+					
+                                      //if both read and write pointer will roll over, we will set rol_over = 0;
+									   //coz its like original condition.
+
+			end
+        //Exclusive Write section
+		else if(write_permitted && !read_permitted)
             begin
+				
                 write_pointer <= write_pointer + 1;
-                empty <= 0;
                 mem[write_pointer] <= data_in; 
+				if(write_pointer == DEPTH-1)//way to handel roll-over.
+					roll_over <= 1'b1; //above if contain only this statement, coz we didnt use 'begin-end'.
+				data_out_valid <= 1'b0; 	                     
             end
-        if(read_permitted)
-            begin
-             read_pointer <= read_pointer + 1;
+		//Exclusive Read Section
+        else if(read_permitted && !write_permitted)
+			begin
+			if(read_pointer == DEPTH-1) //
+					roll_over <= 1'b0;//above if contain only this statement, coz we didnt use 'begin-end'.
+					                   //its not possbl that, read will roll_over and write will not roll-over.
+					
+                                      //if both read and write pointer will roll over, we will set rol_over = 0;
+									   //coz its like original condition.
+			 read_pointer <= read_pointer + 1;
              data_out <= mem[read_pointer];
+			 data_out_valid <= 1'b1; //whenever there is data_out we use valid-bit to differentiate between 'actual data'
+										//and 'null data' or 'unwanted previous data' .
             end
-     
+		else
+			data_out_valid <= 1'b0; 
     end 
 
 endmodule
